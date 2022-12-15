870e12d2e5d60d040fe1d4d8d2ba54031ffc0a53
Muhammad Rasheed <rasheed24291@gmail.com>
2022-12-15 19:07:19 +0500

    Add files via upload

---
+++ question6to10.js
@@ -0,0 +1,55 @@
+// Question No 6
+let varUserName = " \t Muhammad Rasheed\n ";
+console.log("Name With Spaces"+ varUserName);
+let varUserName1 = varUserName .replace(/^\s+|\s+$/gm,'');
+console.log("Name With out Sapces " + varUserName1);
+
+// Question No 7
+//Number Eight: Write addition, subtraction, multiplication, and division
+// Addition
+let varA = 3;
+let varB = 5;
+let result = varA+ varB;
+console.log("Addition: " + result);
+
+// Subtraction of two number
+let varAa = 12;
+let varBb = 4;
+let resultSub = varAa - varBb;
+console.log("Subtraction: " + result);
+
+// Multiplication of two number
+let varMultiA = 2;
+let varMultiB = 4;
+let resultMulti = varMultiA * varMultiB;
+console.log("Multiplication: " + resultMulti);
+
+// Division of two number
+let varDivisionA = 32;
+let varDivisionB = 4;
+let resultDivision = varDivisionA / varDivisionB;
+console.log("Division: " + resultDivision);
+
+// Question No 8
+
+console.log( 4+4 );
+console.log( 10-2 );
+console.log( 4 * 2 );
+console.log( 32/4 );
+
+// Question No 9
+
+var myFavoriteNumber = 16;
+var myMessage = "My Favorite Number is : " + myFavoriteNumber;
+console.log( myMessage );
+
+// Question No 10
+// Adding comments 
+console.log( 4+4 ); // Add two Numbers.
+console.log( 10-2 ); // subtrication operation
+/*** Following are two operation Multplication and Disvision */
+console.log( 4 * 2 );
+console.log( 32/4 );
+
+
+
